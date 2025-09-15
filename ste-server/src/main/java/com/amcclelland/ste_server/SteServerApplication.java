5e1ecc86646e2f8864710b276f646a26e0e6ce9b
aejmcclelland <aejmcclelland@googlemail.com>
2025-09-15 23:17:51 +0100

    chore: scaffold ste-client (Next.js) & ste-server (Spring Boot)

---
+++ ste-server/src/main/java/com/amcclelland/ste_server/SteServerApplication.java
@@ -0,0 +1,13 @@
+package com.amcclelland.ste_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SteServerApplication.class, args);
+	}
+
+}
---
+++ ste-server/src/test/java/com/amcclelland/ste_server/SteServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amcclelland.ste_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SteServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
