ef6cf81d6fc464b4239563c529383fa14331a60c
aejmcclelland <aejmcclelland@googlemail.com>
2025-09-18 22:30:48 +0100

    removed comments

--- ste-server/src/main/java/com/amcclelland/ste_server/config/SecurityConfig.java
+++ ste-server/src/main/java/com/amcclelland/ste_server/config/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig {
                 })
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
-                        // ✅ allow ALL auth endpoints (register, token, future ones)
+
                         .requestMatchers("/api/auth/**").permitAll()
                         // your public health endpoint
                         .requestMatchers("/api/ping").permitAll()
