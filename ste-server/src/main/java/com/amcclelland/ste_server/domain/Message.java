d0183b78e88b082601331e516b3ad01469e4948d
aejmcclelland <aejmcclelland@googlemail.com>
2025-09-18 22:52:51 +0100

    removed unecessary files and code related to PingService and PingController

--- ste-server/src/main/java/com/amcclelland/ste_server/domain/Message.java
+++
@@ -1,8 +0,0 @@
-package com.amcclelland.ste_server.domain;
-
-public record Message(String value) {
-    public Message {
-        if (value == null || value.isBlank())
-            throw new IllegalArgumentException("message cannot be blank");
-    }
-}
